fc4c741c11f32e7a8cfcb262caec799a900319bf
kurravinay <vinay.kurra@gmail.com>
2020-12-31 17:56:47 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,5 @@
-
+import pandas as pd
+def write_parquet_file():
+    df = pd.read_csv('username-password-recovery-code.csv')
+    df.to_parquet('username-password-recovery-code.parquet')
+write_parquet_file()
